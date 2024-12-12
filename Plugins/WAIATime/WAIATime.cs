a72d7f5948256139901ba5b2229f610a624ba109
decipher <dennis.heine@gmx.de>
2024-12-12 04:47:07 +0100

    added waia sample plugin

---
+++ Plugins/WAIATime/WAIATime.cs
@@ -0,0 +1,11 @@
+﻿namespace WAIATime
+{
+    public class WAIATime : WAIA_Plugin.IWAIAPlugin
+    {
+        public string RunPlugin(string text, string[] parameter)
+        {
+            String language = parameter[0];
+            return "translate the following text to " + language + " language. Die Zeit ist " + DateTime.Now.Hour + " uhr und" + DateTime.Now.Minute+" minuten. don't say that you are translating.";
+        }
+    }
+}
